22413dc7f23a4f52f0e0e9789b9c7555e59b102e
Joshua Rosales <joshuarosalesmoreno@hotmail.com>
2026-02-09 20:54:04 -0500

    v2.8.3: Optimización UX - Eliminar alert de confirmación y comprimir modal de detalles

--- Views/Index.vue
+++ Views/Index.vue
@@ -588,12 +588,6 @@
                                 <h2 class="modal-title">DETALLE DEL REPORTE</h2>
                                 <p class="modal-subtitle">Información completa del problema reportado</p>
                             </div>
-                            <button @click="closeReporteDetail" class="modal-close-btn">
-                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                    <line x1="18" y1="6" x2="6" y2="18"></line>
-                                    <line x1="6" y1="6" x2="18" y2="18"></line>
-                                </svg>
-                            </button>
                         </div>
                         
                         <div class="modal-body" v-if="selectedReporte">
@@ -1243,7 +1237,6 @@ const confirmReport = async () => {
 
         const data = await response.json();
         if (data.success) {
-            alert('✓ Reporte enviado correctamente');
             closeReportModal();
             fetchReportes();
             currentTab.value = 'reportes';
